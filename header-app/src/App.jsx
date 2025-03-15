d636cb45572ec8b7616a807374eccba540eca695
smalik21 <sahilmalik25007@gmail.com>
2025-03-15 23:56:44 +0530

    init

---
+++ header-app/src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="HeaderApp">
+      <div>Header</div>
+    </div>
+  );
+}
+
+export default App;
---
+++ header-app/webpack.config.cjs
@@ -0,0 +1,62 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const { dependencies } = require("./package.json");
+
+module.exports = {
+  entry: "./src/entry",
+  mode: "development",
+  devServer: {
+    port: 3001,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env", "@babel/preset-react"],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+    new ModuleFederationPlugin({
+      name: "HeaderApp", // This application named 'HeaderApp'
+      filename: "remoteEntry.js", // output a js file
+      exposes: {
+        // which exposes
+        "./Header": "./src/App", // a module 'Header' from './src/App'
+      },
+      shared: {
+        // and shared
+        ...dependencies, // some other dependencies
+        react: {
+          // react
+          singleton: true,
+          requiredVersion: dependencies["react"],
+        },
+        "react-dom": {
+          // react-dom
+          singleton: true,
+          requiredVersion: dependencies["react-dom"],
+        },
+      },
+    }),
+  ],
+  resolve: {
+    extensions: [".js", ".jsx"],
+  },
+  target: "web",
+};
